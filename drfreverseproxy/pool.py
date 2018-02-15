d2106e070f87c231dded67d1632d9f76bd3760ad
MaxBo <max.bohnet@tuhh.de>
2018-02-15 18:44:17 +0100

    adjust pool.py to urllib3>=1.21

--- drfreverseproxy/pool.py
+++ drfreverseproxy/pool.py
@@ -10,21 +10,32 @@ pool_classes_by_scheme = {
 
 
 class PoolManager(PoolManager_):
-    def _new_pool(self, scheme, host, port):
+    def _new_pool(self, scheme, host, port, request_context=None):
         """
         Create a new :class:`ConnectionPool` based on host, port and scheme.
         This method is used to actually create the connection pools handed out
         by :meth:`connection_from_url` and companion methods. It is intended
         to be overridden for customization.
+
+        If ``request_context`` is provided, it is provided as keyword arguments
+        to the pool class used. This method is used to actually create the
+        connection pools handed out by :meth:`connection_from_url` and
+        companion methods. It is intended to be overridden for customization.
         """
 
         pool_cls = pool_classes_by_scheme[scheme]
-        kwargs = self.connection_pool_kw
+        if request_context is None:
+            request_context = self.connection_pool_kw.copy()
+
+        # Although the context has everything necessary to create the pool,
+        # this function has historically only used the scheme, host, and port
+        # in the positional args. When an API change is acceptable these can
+        # be removed.
+        for key in ('scheme', 'host', 'port'):
+            request_context.pop(key, None)
 
         if scheme == 'http':
-            kwargs = self.connection_pool_kw.copy()
-
             for kw in SSL_KEYWORDS:
-                kwargs.pop(kw, None)
+                request_context.pop(kw, None)
 
-        return pool_cls(host, port, **kwargs)
+        return pool_cls(host, port, **request_context)
